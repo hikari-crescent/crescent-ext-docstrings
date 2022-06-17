acf44889df4c9a6f1c32bf5bf2fe916e974202e3
Lunarmagpie <Bambolambo0@gmail.com>
2022-06-17 02:21:19 -0400

    fix nox

--- noxfile.py
+++ noxfile.py
@@ -4,23 +4,23 @@ import nox
 @nox.session
 def flake8(session):
     session.install("flake8")
-    session.run("flake8", "bot")
+    session.run("flake8", "crescent")
 
 
 @nox.session
 def codespell(session):
     session.install("codespell")
-    session.run("codespell", "bot")
+    session.run("codespell", "crescent")
 
 
 @nox.session
 def mypy(session):
     session.install("poetry")
     session.run("poetry", "install")
-    session.run("mypy", "bot")
+    session.run("mypy", "crescent")
 
 
 @nox.session
 def isort(session):
     session.install("isort")
-    session.run("isort", "--check", "bot")
+    session.run("isort", "--check", "crescent")
