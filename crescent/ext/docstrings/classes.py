e41817c67bc96f9a5809c0b0eeb6f3ed3fa32b28
Lunarmagpie <Bambolambo0@gmail.com>
2022-06-18 21:36:09 -0400

    fix memory leak

--- crescent/ext/docstrings/classes.py
+++ crescent/ext/docstrings/classes.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
+import sys
 
 from typing import Any
+from weakref import finalize
 
 from crescent.commands import decorators
 
@@ -9,13 +11,13 @@ CLASS_DOCSTRINGS: dict[Any, str | None] = {}
 
 cmd_dec = decorators.command
 
-
 def cursed_command_dec(cls_or_func: Any, *args: Any, **kwargs: Any) -> Any:
 
     meta = cmd_dec(cls_or_func, *args, **kwargs)
 
     if isinstance(cls_or_func, type):
         CLASS_DOCSTRINGS[id(meta)] = cls_or_func.__doc__
+        finalize(meta, CLASS_DOCSTRINGS.pop, id(meta))
 
     return meta
 
