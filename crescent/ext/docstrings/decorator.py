9d9077ec1ba3b106823b9746f5216a26bb358cf9
Lunarmagpie <Bambolambo0@gmail.com>
2022-08-23 01:00:40 -0400

    support crescent 0.3.0

--- crescent/ext/docstrings/decorator.py
+++ crescent/ext/docstrings/decorator.py
@@ -3,65 +3,62 @@ from __future__ import annotations
 from functools import partial
 from typing import Callable, overload
 
-from crescent.internal import AppCommandMeta, MetaStruct
+from crescent.internal import AppCommand, AppCommandMeta, Includable
 from docstring_parser import DocstringStyle, parse
 from hikari import CommandOption
 
-from crescent import CommandCallbackT
-
-from .classes import CLASS_DOCSTRINGS
 from .exceptions import ParsingError
 from .style import Style
 
 
-def _lookup_arg(name: str, app: AppCommandMeta) -> CommandOption | None:
-    if not app.app.options:
+def _lookup_arg(name: str, app_command: AppCommand) -> CommandOption | None:
+    if not app_command.options:
         return None
-    for option in app.app.options:
+    for option in app_command.options:
         if option.name == name:
             return option
     return None
 
 
-META_T = MetaStruct[CommandCallbackT, AppCommandMeta]
+IncludableT = Includable[AppCommandMeta]
 
 
 @overload
-def parse_doc(meta: META_T, /) -> META_T:
+def parse_doc(includable: IncludableT, /) -> IncludableT:
     ...
 
 
 @overload
-def parse_doc(*, style: Style = ...) -> Callable[[META_T], META_T]:
+def parse_doc(*, style: Style = ...) -> Callable[[IncludableT], IncludableT]:
     ...
 
 
 def parse_doc(
-    meta: META_T | None = None, style: Style = Style.AUTO
-) -> META_T | Callable[[META_T], META_T]:
+    includable: IncludableT | None = None, style: Style = Style.AUTO
+) -> IncludableT | Callable[[IncludableT], IncludableT]:
 
-    if meta is None:
+    if includable is None:
         return partial(parse_doc, style=style)
 
-    docs = CLASS_DOCSTRINGS.get(id(meta)) or meta.callback.__doc__
+    docs = includable.metadata.owner.__doc__
 
     if not docs:
-        return meta
+        return includable.metadata.owner
 
     parsed_docs = parse(docs, style=DocstringStyle(style))
 
     for param in parsed_docs.params:
-        app_option = _lookup_arg(param.arg_name, meta.metadata)
+        app_option = _lookup_arg(param.arg_name, includable.metadata.app_command)
         if app_option is None:
             raise ParsingError(
                 f"Parameter `{param.arg_name}` is not present in the signature of"
-                f"`{meta.callback.__name__}`."
+                f"`{includable.metadata.callback.__name__}`."
             )
 
         if param.description:
             app_option.description = param.description
 
     if parsed_docs.short_description:
-        meta.metadata.app.description = parsed_docs.short_description
+        includable.metadata.app_command.description = parsed_docs.short_description
 
-    return meta
+    return includable
